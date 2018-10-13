bdd32490740f72932907d6ba6eaab1f42369c1a0
smoukiDev <smakfactory@gmail.com>
2018-10-14 01:37:06 +0300

    Disable ConnectForm Resize

--- WinFormUI/ConnectForm.Designer.cs
+++ WinFormUI/ConnectForm.Designer.cs
@@ -146,6 +146,7 @@
             this.Controls.Add(this.label1);
             this.Controls.Add(this.txtDataSource);
             this.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
             this.Margin = new System.Windows.Forms.Padding(6);
             this.Name = "ConnectForm";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
